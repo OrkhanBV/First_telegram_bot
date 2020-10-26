9e4f41ffa7353d59863eab4574a941ecbf68eae7
OrkhanBV <babaev.or@yandex.ru>
2020-10-26 16:21:26 +0300

    iventhandler class added

--- telegram/telegram/IventHandler.cs
+++ telegram/telegram/IventHandler.cs
@@ -1,26 +1,8 @@
-using System;
+
 using Telegram.Bot;
 using System.Threading.Tasks;
 using Telegram.Bot.Types;
 
-/*
- * System.Threading.Tasks
- *
- * Task - тип возвращаемого значения при асинхронном программировани более подробно см по ссылке
- * https://docs.microsoft.com/ru-ru/dotnet/csharp/programming-guide/concepts/async/async-return-types
- * 
- * async
- * await
- * 
- * Всё вышеперечисленное относится к асинхронному программированию
- * Более подробно с примером асинхронного программирования можно ознакомиться
- * по ссылке:
- *
- * https://docs.microsoft.com/ru-ru/dotnet/csharp/programming-guide/concepts/async/
- * 
- */
-
-/*
 namespace telegram
 {
     public class IventHandler
@@ -61,41 +43,5 @@ namespace telegram
                         "Второе : https://metanit.com/sharp/entityframework/");
                 }
         }
-    }
-}
-*/
-
-namespace telegram
-{
-    public class BroKhanBot
-    {
-            private const string TOKEN = "1307823169:AAEhyMxnJXAnQJWONwCg2keQFbAW2-j4NTA";
-            public static async Task GetMessages()
-            {
-                TelegramBotClient bot = new TelegramBotClient(TOKEN);
-                KeyboardButtons buttons = new KeyboardButtons();
-                IventHandler iventHandler = new IventHandler();
-                int offset = 0;
-                int timeout = 0;
-                try
-                {
-                    await bot.SetWebhookAsync("");
-                    while (true)
-                    {
-                        var updates = await bot.GetUpdatesAsync(offset, timeout);
-                        foreach (var update in updates)
-                        {
-                            await buttons.create_buttons(bot, update);
-                            await iventHandler.getIventSendLink(bot, update);
-                            offset = update.Id + 1;
-                        }
-                    }
-                }
-                catch (Exception e)
-                {
-                    Console.WriteLine("Error :" + e);
-                    throw;
-                }
-            }
     }
 }
